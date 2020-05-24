01778552e5f65f95e844127dac8c4df734586827
Rodrigo Adão dos Santos <rodrigoadaodossantos@gmail.com>
2020-05-24 17:04:36 -0300

    Simple react-app

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import "./styles.css";
+const Header = () => {
+    return(
+        <header id="main-header">JSHunt</header>
+    )
+}
+
+export default Header
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import Axios from 'axios'
+
+const api = Axios.create({baseURL: 'https:rocketseat-node.herokuapp.com/api' })
+
+
+export default api
